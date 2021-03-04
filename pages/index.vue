78f3f4d01b63aa8de10f7e114d3656661c2ba54c
simsu <dvt18@naver.com>
2021-03-04 16:18:33 +0900

    영수증 html css

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,270 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div class="hcr">
+    <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%;background-color:#f8f8f9;letter-spacing:-1px">
+      <tbody>
+        <tr>
+          <td align="center">
+            <div style="max-width:595px; margin:0 auto">
+              <table cellpadding="0" cellspacing="0" style="width:100%;margin:0 auto;background-color:#fff;;-webkit-text-size-adjust:100%;text-align:left">
+                <tbody>
+                  <tr>
+                    <td colspan="3" height="30"></td>
+                  <tr>
+                    <td width="21"></td>
+                    <td>
+                      <table cellpadding="0" cellspacing="0" style="width:100%;margin:0;padding:0">
+                        <tbody>
+                          <tr>
+                            <td style="margin:0;padding:0">
+                              <a href="#" target="_blank">
+                                <img src="http://order.pay.naver.com/static/img/service/front/order/img_ems/npUser/logo.gif" width="51" height="20" alt="N Pay" style="border:0;margin-right:5px;" loading="lazy">
+                              </a>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td height="11"></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                    <td width="21"></td>
+                  </tr>
+                  <tr>
+                    <td width="21"></td>
+                    <td>
+                      <table cellpadding="0" cellspacing="0" style="width:100%;margin:0;padding:0"> 
+                        <tbody>
+                          <tr>
+                            <td style="font-size:28px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#424240;line-height:34px;vertical-align:top">
+                              네이버페이로
+                              <br>
+                              <span style="color:#1ec800">
+                                 주문하신 내역
+                              </span>
+                              입니다.
+                            </td>
+                          </tr>
+                          <tr><td height="13"></td></tr>
+                          <tr><td height="22"></td></tr>
+                        </tbody>
+                      </table>
+                    </td>
+                    <td width="21"></td>
+                  </tr>
+                  <tr>
+                    <td colspan="3" height="1" style="background:#e5e5e5"></td>
+                  </tr>
+                  <tr>
+                    <td colspan="3" height="26"></td>
+                  </tr>
+                  <tr>
+                    <td width="21"></td>
+                    <td>
+                      <table cellpadding="0" cellspacing="0" style="width:100%;margin:0;padding:0"> 
+                        <tbody>
+                          <tr>
+                            <td width="70" style="padding-bottom:9px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#696969;vertical-align:top">
+                              고객명
+                            </td>
+                            <td style="padding-bottom:9px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#333;vertical-align:top">
+                              심*연님
+                            </td>
+                          </tr>
+                          <tr>
+                            <td width="70" style="padding-bottom:9px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#696969;vertical-align:top">
+                              주문번호
+                            </td>
+                            <td style="padding-bottom:7px;font-size:17px;font-family:Helvetica;font-weight:bold">
+                              <a target="_blank" href="https://order.pay.naver.com/orderStatus/2021020470544290" style="color:#1ec800;text-decoration:none" rel="noreferrer noopener">2021020470544290</a>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td width="70" style="padding-bottom:9px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#696969;vertical-align:top">
+                              주문일자
+                            </td>
+                            <td style="padding-bottom:9px;font-size:14px;font-family:Helvetica;color:#333;vertical-align:top">
+                              2021.02.04 18:24
+                            </td>
+                          </tr>
+                          <tr>
+                            <td colspan="2" height="47"></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                    <td width="21"></td>
+                  </tr>
+                  <tr>
+                    <td width="21"></td>
+                    <td>
+                      <table cellpadding="0" cellspacing="0" style="width:100%;margin:0;padding:0">
+						            <tbody>
+                          <tr>
+                            <td height="24" style="font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;font-weight:bold;color:#000;vertical-align:top">
+                              결제상세
+                            </td>
+                          </tr>
+                          <tr>
+                            <td height="2" style="background:#424240"></td>
+                          </tr>
+                          <tr>
+                            <td height="18"></td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <table cellpadding="0" cellspacing="0" style="width:100%;margin:0;padding:0">
+                                <tbody>
+                                  <tr>
+                                    <td width="50%" style="padding-bottom:15px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#696969;line-height:16px;vertical-align:top">
+                                    주문금액
+                                    </td>
+                                    <td style="padding-bottom:15px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#000;line-height:16px;text-align:right;vertical-align:top">
+                                      <span style="font-family:Helvetica;line-height:20px">16,800</span>원
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td width="50%" style="padding-top:15px;border-top: 1px dotted #e6e6e6;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#696969;line-height:16px;vertical-align:top">
+                                      포인트 사용
+                                    </td>
+                                    <td style="padding-top:15px;border-top: 1px dotted #e6e6e6;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#000;line-height:16px;text-align:right;vertical-align:top">
+                                      <span style="font-family:Helvetica;line-height:20px">16,800</span>원
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td width="50%" style="padding-top:8px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#696969;line-height:16px;vertical-align:top">
+                                      <span style="display: inline-block;vertical-align: top;width: 6px; height: 6px;margin: 4px 5px 0 0;border: solid #ddd;border-width: 0 0 2px 2px;"></span>
+                                      적립 포인트
+                                    </td>
+                                    <td style="padding-top:8px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#666;line-height:16px;text-align:right;vertical-align:top">
+                                      <span style="font-family:Helvetica;line-height:20px">3,471</span>원
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td width="50%" style="padding-top:8px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#696969;line-height:16px;vertical-align:top">
+                                      <span style="display: inline-block;vertical-align: top;width: 6px; height: 6px;margin: 4px 5px 0 0;border: solid #ddd;border-width: 0 0 2px 2px;"></span>
+                                        충전 포인트
+                                    </td>
+                                    <td style="padding-top:8px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#666;line-height:16px;text-align:right;vertical-align:top">
+                                      <span style="font-family:Helvetica;line-height:20px">13,329</span>원
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td height="18"></td>
+                                  </tr>
+                                  <tr>
+                                    <td colspan="2" height="1" style="background:#d5d5d5"></td>
+                                  </tr>
+                                </tbody>
+                              </table>
+							              </td>
+                          </tr>
+                        </tbody>
+					            </table>
+                    </td>
+                    <td width="21"></td>
+                  </tr>
+                  <tr>
+                    <td width="21"></td>
+                    <td>
+                      <table cellpadding="0" cellspacing="0" style="width:100%;margin:0;padding:0"> 
+						            <tbody>
+                          <tr>
+                            <td height="56"></td>
+                          </tr>
+                          <tr>
+                            <td height="23" style="font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;font-weight:bold;color:#000;vertical-align:top">
+                              배송정보
+                            </td>
+                          </tr>
+                          <tr>
+                            <td height="2" style="background:#424240"></td>
+                          </tr>
+                          <tr>
+                            <td height="20"></td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <table cellpadding="0" cellspacing="0" style="width:100%;margin:0;padding:0"> 
+                                <tbody>
+                                  <tr>
+                                    <td width="70" style="padding-bottom:5px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#696969;line-height:23px;vertical-align:top">
+                                      수령인
+                                    </td>
+                                    <td style="padding-bottom:5px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#000;line-height:23px;vertical-align:top">
+                                      심*연
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td width="70" style="padding-bottom:5px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#696969;line-height:23px;vertical-align:top">
+                                      연락처
+                                    </td>
+                                    <td style="padding-bottom:5px;font-size:14px;font-family:Helvetica;color:#000;line-height:23px;vertical-align:top">
+                                      010-9965-****
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td width="70" style="padding-bottom:6px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#696969;line-height:23px;vertical-align:top">
+                                      배송지
+                                    </td>
+                                    <td style="padding-bottom:6px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#000;line-height:23px;vertical-align:top">
+                                      27483<br>
+                                      충청북도 충주시 호암중앙1로 22-1 (호암동, 동평아트빌라)<br>
+                                      나동 ****
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td width="70" style="padding-bottom:5px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#696969;line-height:23px;vertical-align:top">
+                                      배송메모
+                                    </td>
+                                    <td style="padding-bottom:5px;font-size:14px;font-family:'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,'돋움',Dotum,Helvetica,'Apple SD Gothic Neo',Sans-serif;color:#000;line-height:23px;vertical-align:top">
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td colspan="2" height="1" style="background:#d5d5d5"></td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                    <td width="21"></td>
+                  </tr>
+                  <tr>
+                    <td width="21"></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+<style>
+.hcr {
+  word-wrap: break-word;
+  word-break: normal;
+  line-height: 1.5;
+  position: relative;
+  margin: 0;
+  padding: 20px 33px 40px 39px;
+  zoom: 1;
+  color: #000;
 }
-</script>
+.hcr:after{
+  clear: both;
+  height: 0;
+  visibility: hidden;
+  display: block;
+}
+table {
+  border-color: grey;
+}
+a, a:link {
+    text-decoration: none;
+    background-color: transparent;
+}
+</style>
